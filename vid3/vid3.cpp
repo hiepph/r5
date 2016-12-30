041e23505167dac0127547425146faa5c757e0e5
hiepph <hoanghiepjp96@gmail.com>
2016-12-30 16:05:38 +0700

    refactor(vid3): Update flow

--- vid3/vid3.cpp
+++ vid3/vid3.cpp
@@ -1,152 +1,165 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define _CRT_SECURE_NO_DEPRECATE
-
+#include <opencv2/opencv.hpp>
 #include <iostream>
 #include <queue>
 #include <algorithm>
-#include "opencv2\core\core.hpp"
-#include "opencv2\highgui\highgui.hpp"
-#include "opencv2\opencv.hpp"
-
 
 using namespace std;
 using namespace cv;
 
 bool countour_area_comparision(Moments &a, Moments &b) {
-	return a.m00 > b.m00;
+  return a.m00 > b.m00;
 }
 
 Point2d get_lane_largest_mass_center(Mat &frame, double brightness_dec, int gblur_ksize, int gblur_sigma_x, double thresh, double width_offset) {
-	int x = brightness_dec;
-	frame += Scalar(-x, -x, -x);
+  int x = brightness_dec;
+  frame += Scalar(-x, -x, -x);
 
-	GaussianBlur(frame, frame, Size(gblur_ksize, gblur_ksize), gblur_sigma_x);
+  GaussianBlur(frame, frame, Size(gblur_ksize, gblur_ksize), gblur_sigma_x);
 
-	Mat canny_output;
-	vector<vector<Point> > contours;
-	vector<Vec4i> hierarchy;
-	threshold(frame, canny_output, thresh, 255, THRESH_BINARY);
+  Mat canny_output;
+  vector<vector<Point> > contours;
+  vector<Vec4i> hierarchy;
+  threshold(frame, canny_output, thresh, 255, THRESH_BINARY);
 
-	// Find contours
-	findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
-	if (contours.size() < 1) {
-		frame = Mat::zeros(canny_output.size(), CV_8UC3);
-		return Point2d(width_offset, 0);
-	}
+  // Find contours
+  findContours(canny_output, contours, hierarchy, CV_RETR_TREE, CHAIN_APPROX_SIMPLE, Point(0, 0));
+  if (contours.size() < 1) {
+    frame = Mat::zeros(canny_output.size(), CV_8UC3);
+    return Point2d(width_offset, 0);
+  }
 
-	// Get the moments
-	vector<Moments> mu(contours.size());
-	for (int i = 0; i < contours.size(); ++i) {
-		mu[i] = moments(contours[i], false);
-	}
+  // Get the moments
+  vector<Moments> mu(contours.size());
+  for (size_t i = 0; i < contours.size(); ++i) {
+    mu[i] = moments(contours[i], false);
+  }
 
-	// Manually skipping light
-	sort(mu.begin(), mu.end(), countour_area_comparision);
+  // Manually skipping light
+  sort(mu.begin(), mu.end(), countour_area_comparision);
 
-	// Get the mass centers
-	vector<Point2d> mc(contours.size());
-	for (int i = 0; i < contours.size(); ++i) {
-		mc[i] = Point2d(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
-	}
+  // Get the mass centers
+  vector<Point2d> mc(contours.size());
+  for (size_t i = 0; i < contours.size(); ++i) {
+    mc[i] = Point2d(mu[i].m10 / mu[i].m00, mu[i].m01 / mu[i].m00);
+  }
 
-	// Draw contours
-	Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
-	for (int i = 0; i < contours.size(); ++i) {
-		Scalar color = Scalar(0, 255, 0);
-		drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
-		circle(drawing, mc[i], 4, color, -1, 8, 0);
-	}
+  // Draw contours
+  Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
+  for (size_t i = 0; i < contours.size(); ++i) {
+    Scalar color = Scalar(0, 255, 0);
+    drawContours(drawing, contours, i, color, 2, 8, hierarchy, 0, Point());
+    circle(drawing, mc[i], 4, color, -1, 8, 0);
+  }
 
-	frame = drawing;
+  frame = drawing;
 
-	mc[0].x += width_offset;
-	return mc[0];
+  mc[0].x += width_offset;
+  return mc[0];
 }
 
-int main() {
-	VideoCapture cap("test3.avi");
+int main(int argc, char** argv) {
+  /**
+   * Read input video from first command line argument
+   *      $ ./prog <video_path>
+   *
+   * Break program immediately and return -1 if no input video specified,
+   * or if something error happened (wrong type of video)
+   *
+   */
 
-	int frame_count = cap.get(CV_CAP_PROP_FRAME_COUNT);
-	cout << frame_count << endl;
+  if (argc != 2) {
+      printf("error: %s <video_path>", argv[0]);
+      return -1;
+  }
 
-	Mat fin;
-	int current_frame = 0;
-	Point2d prev_point, mid_point;
-	namedWindow("demo1", WINDOW_AUTOSIZE);
-	namedWindow("source", WINDOW_AUTOSIZE);
+  string video = argv[1];
+  VideoCapture cap(video);
+  if (!cap.isOpened()) {
+      cout << "error reading " << video << endl;
+      return -1;
+  }
 
-	while (1) {
-		// If cannot read the video, which means it is the end of video!
-		Mat frame, temp;
-		cap >> frame;
-		if (frame.empty()) {
-			break;
-		}
+  int frame_count = cap.get(CV_CAP_PROP_FRAME_COUNT);
+  cout << frame_count << endl;
 
-		cout << "Frame " << current_frame << ":" << endl;
+  Mat fin;
+  int current_frame = 0;
+  Point2d prev_point, mid_point;
+  namedWindow("demo1", WINDOW_AUTOSIZE);
+  namedWindow("source", WINDOW_AUTOSIZE);
 
-		// We only focus at 1/4 bottom of video, so crop frame
-		// Remember to add the offset 3/4 of row (y)
-		double row_offset = (double)frame.rows * 3 / 4;
-		frame = frame(Range(frame.rows * 3 / 4, frame.rows), Range::all());
-		temp = frame;
+  while (1) {
+    // If cannot read the video, which means it is the end of video!
+    Mat frame, temp;
+    cap >> frame;
+    if (frame.empty()) {
+      break;
+    }
 
-		// Grayscale
-		cvtColor(frame, frame, CV_BGR2GRAY);
+    cout << "Frame " << current_frame << ":" << endl;
 
-		// Split frame into 2 halves
-		vector<Mat> half;
-		half.push_back(frame(Rect(0, 0, frame.cols / 2, frame.rows)).clone());
-		half.push_back(frame(Rect(frame.cols / 2, 0, frame.cols / 2, frame.rows)).clone());
+    // We only focus at 1/4 bottom of video, so crop frame
+    // Remember to add the offset 3/4 of row (y)
+    // double row_offset = (double)frame.rows * 3 / 4;
+    frame = frame(Range(frame.rows * 3 / 4, frame.rows), Range::all());
+    temp = frame;
 
-		// Process each half(lane) and get center of lane
-		Point2d
-			left_lane_center = get_lane_largest_mass_center(half[0], 48, 57, 1, 80, 0),
-			right_lane_center = get_lane_largest_mass_center(half[1], 53, 17, 3, 115, frame.cols / 2);
-		hconcat(half[0], half[1], fin);
+    // Grayscale
+    cvtColor(frame, frame, CV_BGR2GRAY);
 
-		// Get (x, y) of the  midpoint
-		// And draw it
-		Scalar mid_color = Scalar(0, 0, 255);
-		mid_point = (left_lane_center + right_lane_center) * 0.5;
-		cout << "\tmid: (" << mid_point.x << ", " << mid_point.y << ")" << endl;
+    // Split frame into 2 halves
+    vector<Mat> half;
+    half.push_back(frame(Rect(0, 0, frame.cols / 2, frame.rows)).clone());
+    half.push_back(frame(Rect(frame.cols / 2, 0, frame.cols / 2, frame.rows)).clone());
 
-		// Rescue problem frame
-		if (current_frame != 0) {
-			double dx = abs(mid_point.x - prev_point.x);
-			double dy = abs(mid_point.y - prev_point.y);
+    // Process each half(lane) and get center of lane
+    Point2d
+    left_lane_center = get_lane_largest_mass_center(half[0], 48, 57, 1, 80, 0),
+    right_lane_center = get_lane_largest_mass_center(half[1], 53, 17, 3, 115, frame.cols / 2);
+    hconcat(half[0], half[1], fin);
 
-			cout << "\tdx = " << dx << ", " << "dy = " << dy << endl;
+    // Get (x, y) of the  midpoint
+    // And draw it
+    Scalar mid_color = Scalar(0, 0, 255);
+    mid_point = (left_lane_center + right_lane_center) * 0.5;
+    cout << "\tmid: (" << mid_point.x << ", " << mid_point.y << ")" << endl;
 
-			if (dx > 7.0) {
-				cerr << "CRASH!!! Reuse previous point" << endl;
-				mid_point = prev_point;
-			}
-		}
+    // Rescue problem frame
+    if (current_frame != 0) {
+      double dx = abs(mid_point.x - prev_point.x);
+      double dy = abs(mid_point.y - prev_point.y);
 
-		// ..draw
-		circle(fin, mid_point, 4, mid_color, -1, 8, 0);
-		circle(temp, mid_point, 4, mid_color, -1, 8, 0);
-		
-		// Show frame to windows
-		imshow("demo1", fin);
-		imshow("source", temp);
+      cout << "\tdx = " << dx << ", " << "dy = " << dy << endl;
 
-		// Wait to display frame
-		waitKey(20);
+      if (dx > 7.0) {
+        cerr << "CRASH!!! Reuse previous point" << endl;
+        mid_point = prev_point;
+      }
+    }
 
-		cout << endl;
+    // ..draw
+    circle(fin, mid_point, 4, mid_color, -1, 8, 0);
+    circle(temp, mid_point, 4, mid_color, -1, 8, 0);
 
-		// Increate index of frame
-		current_frame++;
+    // Show frame to windows
+    imshow("demo1", fin);
+    imshow("source", temp);
 
-		// Update prev point for interpolating (prevent problem frame)
-		prev_point = mid_point;
+    // Wait to display frame
+    waitKey(20);
 
-		waitKey(20);
-	}
+    cout << endl;
 
-	cap.release();
+    // Increate index of frame
+    current_frame++;
 
-	return 0;
+    // Update prev point for interpolating (prevent problem frame)
+    prev_point = mid_point;
+
+    waitKey(20);
+  }
+
+  cap.release();
+
+  return 0;
 }
